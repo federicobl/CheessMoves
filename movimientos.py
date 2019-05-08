613a973a3772b26f739835ff825e071bf874f8c8
jbuenof45401 <jbuenof45401@universidadean.edu.co>
2019-05-07 20:06:54 -0500

    desarrollo matriz

--- movimientos.py
+++ movimientos.py
@@ -15,16 +15,7 @@ def tablero_a_cadena(tablero):
     recibimos un tablero o diccionario y devolvemos una cadena
 
     >>> tablero_a_cadena(tablero)
-    [
-    ['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    ['T', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']
-    ]
+
 
 
     :param tablero: el tablero con la posicionde cada ficha
@@ -71,4 +62,40 @@ def obtener_nombre_pieza(simbolo):
 
 
 def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
-    pass
+    '''
+    ([][],int,int,int,int)-> [][]: tablero resultante.
+
+
+    :param tablero: [][]: matriz con la posicion de las fichas
+    :param x_inicial: int: entero indocando posicion en x inicial
+    :param y_inicial: int: entero indocando posicion en y inicial
+    :param x_final: int: entero indocando posicion en x final
+    :param y_final: int: entero indocando posicion en y final
+    :return: [][] tablero resultante
+    '''
+
+    ficha =  tablero[x_inicial][y_inicial]
+
+    x_diferencia =  x_final- x_inicial
+    y_diferencia = y_final - y_inicial
+
+    cont_filas = 0
+    cont_columnas = 0
+    for fila in range(x_inicial,x_final):
+        if(x_inicial == x_final):
+            break
+        if fila != x_inicial and fila != x_final and tablero[fila][y_inicial] != ' ':
+            return 'camino bloqueado'
+        if fila == x_final:
+            tablero[fila][y_inicial] = tablero[x_inicial,x_final]
+
+    for columna in range(y_inicial,y_final):
+        if(tablero[x_inicial][columna])!= ' ':
+            return 'camino bloqueado'
+
+
+
+    if (ficha != 't'):
+        return 'no es una torre'
+print(mover_torre(tablero, 1, 1, 2, 2))
+
